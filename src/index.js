6a09c63041e8df19855ed8a1e915bf6957cc6a27
tuanlm <tuanlmst02@gmail.com>
2025-10-16 21:07:04 +0700

    Update source edit course detail

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -16,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 //HTTP logger
 app.use(morgan('combined'));
@@ -25,6 +27,9 @@ app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -6,6 +6,8 @@ const coursesController = require('../app/controllers/CourseController');
 // newsController.index
 router.get('/create', coursesController.create);
 router.post('/store', coursesController.store);
+router.get('/:id/edit', coursesController.edit);
+router.put('/:id', coursesController.update);
 router.get('/:slug', coursesController.show);
 
 module.exports = router;
