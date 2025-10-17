5e80d4a08685d5923503f80b2e0e919db945d363
tuanlm <tuanlmst02@gmail.com>
2025-10-17 22:16:39 +0700

    Update delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -48,6 +48,13 @@ class CourseController {
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
+
+    // [DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
